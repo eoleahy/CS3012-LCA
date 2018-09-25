901c64fc6488f36ede05f9924d7d47cb00a8a3f7
eoleahy1 <eoleahy@tcd.ie>
2018-09-25 20:17:09 +0100

    Added a test for empty check function and added a test for LCA function to be implemented

--- Main.py
+++ Main.py
@@ -16,5 +16,20 @@ class TestBSTMethods(unittest.TestCase):
         tree.insert(3)
         self.assertFalse(tree.getRoot().val==5)
 
+    def testEmpty(self):
+        tree=Tree()
+        self.assertTrue(tree.isEmpty())
+
+    def testLCA(self):
+        tree=Tree()
+        tree.insert(2)
+        tree.insert(3)
+        tree.insert(1)
+        tree.insert(5)
+        tree.insert(7)
+        tree.insert(1)
+        self.assertTrue(tree.getRoot().val,2)
+        self.assertTrue(tree.lca(tree.getRoot(), 1, 3) == 2)
+
 if __name__ == '__main__':
     unittest.main()
