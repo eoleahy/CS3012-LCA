bd031b6984b2c27a5524367d42a6d3fd26752737
eoleahy1 <eoleahy@tcd.ie>
2018-10-16 16:15:04 +0100

    Began implementation of LCA

--- Main.py
+++ Main.py
@@ -1,38 +1,29 @@
-from BSTDAG import *
+# from BSTDAG import *
 import networkx as nx
 import unittest
 
 class TestBSTMethods(unittest.TestCase):
 
+
+    def lca(G,source,a,b):
+        pathA=nx.single_source_shortest_path(G,source,a)
+        pathB=nx.single_source_shortest_path(G,source,b)
+
+        return (pathA)
+
     def setUp(self):
         pass
 
-    def testInsert(self):
-        print("Branch")
-        tree=Tree()
-        tree.insert(5)
-        self.assertEqual(tree.getRoot().val,5)
-
-    def testInsert2(self):
-        tree=Tree()
-        tree.insert(3)
-        self.assertFalse(tree.getRoot().val==5)
-
-    def testEmpty(self):
-        tree=Tree()
-        self.assertTrue(tree.isEmpty())
-
     def testLCA(self):
-        tree=Tree()
-        tree.insert(2)
-        tree.insert(3)
-        tree.insert(1)
-        tree.insert(5)
-        tree.insert(7)
-        tree.insert(1)
-        x = tree.lca(tree.root,1,3)
-        self.assertTrue(x, 2)
-
+        G=nx.DiGraph()
+        G.add_edge(1,2)
+        G.add_edge(2,3)
+        G.add_edge(2,7)
+        paths = TestBSTMethods.lca(G,1,3,7)
+        print(paths)
+        x=3
+        self.assertTrue(3,3)
+"""
     def testLCA2(self):
         tree=Tree()
         tree.insert(5)
@@ -43,6 +34,8 @@ class TestBSTMethods(unittest.TestCase):
         tree.insert(4)
         x = tree.lca(tree.root,2,4)
         self.assertTrue(x, 3)
+"""
+
 
 if __name__ == '__main__':
     unittest.main()
