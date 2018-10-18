ebaf891cd8550e7d96e8216b5b87206981e60531
eoleahy1 <eoleahy@tcd.ie>
2018-10-18 14:47:38 +0100

    Completed 1 test and LCA implementation

--- Main.py
+++ Main.py
@@ -6,10 +6,24 @@ class TestBSTMethods(unittest.TestCase):
 
 
     def lca(G,source,a,b):
-        pathA=nx.single_source_shortest_path(G,source,a)
-        pathB=nx.single_source_shortest_path(G,source,b)
+        paths=nx.single_source_shortest_path(G,source,a)
+        print("All path: ",  paths)
+        pathA = paths[a]
+        pathB = paths[b]
+        print("path A: ", pathA)
+        print("path B: ", pathB)
 
-        return (pathA)
+        lca = None
+
+        for i in reversed(pathA):
+            print("i", i)
+            for j in reversed(pathB):
+                print("j", j)
+                if (i is j):
+                    lca = i
+                    print("LCA is", lca)
+                    return(lca)
+        return (lca)
 
     def setUp(self):
         pass
@@ -17,12 +31,13 @@ class TestBSTMethods(unittest.TestCase):
     def testLCA(self):
         G=nx.DiGraph()
         G.add_edge(1,2)
-        G.add_edge(2,3)
+        G.add_edge(2,4)
+        G.add_edge(4,3)
         G.add_edge(2,7)
-        paths = TestBSTMethods.lca(G,1,3,7)
-        print(paths)
-        x=3
-        self.assertTrue(3,3)
+        lca = TestBSTMethods.lca(G,1,3,7)
+    #    print(paths)
+
+        self.assertTrue(lca,2)
 """
     def testLCA2(self):
         tree=Tree()
